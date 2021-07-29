b1f319968f7321ea483263602f5eb39848289bc9
Richard Elkins <richard.elkins@gmail.com>
2021-07-29 09:59:51 -0500

    Update test_synth_signal.py

--- test_synth_signal.py
+++ test_synth_signal.py
@@ -17,16 +17,16 @@ from main import DopplerFinder
 
 # Define observation parameters
 f_start = 3e9  #Hz
-BW = 1e6 #Hz
+TOTAL_BW = 1e6 #Hz
 tsamp=1 #seconds
-f_stop = f_start + BW
+f_stop = f_start + TOTAL_BW
 n_fine_chans = int(1e6)
 ntime = int(64)
 mjd=59423.2
 
 
 print("Clancy is being initialised")
-clancy = DopplerFinder(filename="CH0_TIMESTAMP",
+clancy = DopplerFinder(filename="CH0_TIMESTAMP.h5",
                        source_name="test",
                        src_raj=7.456805,
                        src_dej=5.225785,
