afb146de84739df4913b6cb0b694c41d246fda48
Richard Elkins <richard.elkins@gmail.com>
2021-07-30 12:10:24 -0500

    Update test_synth_signal.py

--- test_synth_signal.py
+++ test_synth_signal.py
@@ -20,7 +20,7 @@ from main import DopplerFinder
 # Define observation parameters
 n_fine_chans = int(4e6) # setigen fchans
 n_fine_chans = 1024
-ntime = 60 # seconds,     setigen tchans
+ntime = 64 # seconds,     setigen tchans
 f_start = 8437.625 # MHz, setigen fch1
 foff = 1e-6 # MHz,        setigen df
 f_stop = f_start + (n_fine_chans - 1) * foff # MHz
